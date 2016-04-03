747217c98639a63e796ee9d0550052af95679266
fungwan <406029034@qq.com>
2016-04-03 18:15:39 +0800

    update

---
+++ picasaServer/routes/index.js
@@ -0,0 +1,46 @@
+﻿/**
+ * Created by Administrator on 2016/2/3.
+ */
+
+
+var sign = require('../models/signature.js');
+var multiparty = require('connect-multiparty');
+var multipartyMiddleware = multiparty({
+    uploadDir: './'
+});
+
+module.exports = function(app) {
+
+    app.get('/', function(req, res){
+        res.render('index','');
+    });
+
+    //获取签名
+    app.post('/sign', function(req, res){
+
+        var url = req.body.url;
+
+        res.json({
+            result: ''});
+
+        /*sign.getSign(url,function(data){
+
+             res.json({
+             result: data});
+
+        });*/
+
+    });
+
+
+
+    app.post('/upload', multipartyMiddleware,function(req, res){
+
+
+        var file = req.files.file;
+        console.log(file.name);
+        console.log(file.type);
+
+        res.send(201);
+    });
+};
---
+++ picasaServer/uzuooWebsite.js
@@ -0,0 +1,19 @@
+/**
+ * Created by Administrator on 2015/12/15.
+ */
+var express = require('express');
+var path = require('path');
+var app = express();
+var routes = require('./routes');
+var bodyParser = require('body-parser');
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+
+routes(app);
+
+app.listen(8000);
