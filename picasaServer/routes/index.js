636e3479183647f61034b42142607b5d6b742816
fungwan <406029034@qq.com>
2016-04-07 16:10:46 +0800

    update

--- picasaServer/routes/index.js
+++ picasaServer/routes/index.js
@@ -6,7 +6,7 @@
 var sign = require('../models/signature.js');
 var multiparty = require('connect-multiparty');
 var multipartyMiddleware = multiparty({
-    uploadDir: './'
+    uploadDir: './tmp'
 });
 
 module.exports = function(app) {
@@ -33,12 +33,14 @@ module.exports = function(app) {
     });
 
     app.post('/upload', multipartyMiddleware,function(req, res){
-
-
+       
+        var content = req.body;
         var file = req.files.file;
+        console.log(content.name);
         console.log(file.name);
         console.log(file.type);
+        console.log(file.path);
 
-        res.send(201);
+        res.send(file.name);
     });
 };
